359e654aab64d44fe06a4c9591656e9e698a9b88
jayme <jaymeliao1226@gmail.com>
2022-04-01 21:43:53 -0400

    init commit:tab bar button are just seguea

---
+++ AI_Recycle_Helper/MainTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarViewController.swift
+//  AI_Recycle_Helper
+//
+//  Created by Jayme on 2022-03-27.
+//
+
+import UIKit
+
+class MainTabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
